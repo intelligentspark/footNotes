babafa658dee73287273c50b87b0f371e057881d
fbliss <fred.bliss@intelligentspark.com>
2017-12-04 03:09:33 -0500

    added frontend js (jquery-based) that controls footnote behaviors

---
+++ footnotes/frontend.js
@@ -0,0 +1,21 @@
+<script>
+jQuery(document).ready(function() {
+    var footnotes = [];
+    jQuery("span.fnoteWrap").on('click',function(event) {
+        var footnote_content = jQuery(this).children('span.fnoteDesc').html();
+        var fid = jQuery(this).data('footnote-id');
+
+        if(jQuery.inArray(fid,footnotes)==-1) {	//create new footnote content box
+            footnotes.push(fid);
+            jQuery('<div id="wk_ft'+fid+'_content" class="footnoteContent" style="display:none;"><button id="wk_ft'+fid+'_close_button"class="closeBtn">X</button><br>'+footnote_content+'</div>').appendTo(this);
+            jQuery("#wk_ft"+fid+"_close_button").on('click',function(event) {
+                event.stopPropagation();
+                jQuery("#wk_ft"+fid+"_content").fadeOut();
+            });
+
+        }
+        jQuery("#wk_ft"+fid+"_content").fadeIn();
+
+    });
+});
+</script>
